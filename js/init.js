90a094e243a12fa972c79944a1bb74ec16682e89
Matias Souza <matii-s@hotmail.com>
2020-08-21 11:10:06 -0300

    actualizacion

--- js/init.js
+++ js/init.js
@@ -7,41 +7,75 @@ const PRODUCT_INFO_COMMENTS_URL = "https://japdevdep.github.io/ecommerce-api/pro
 const CART_INFO_URL = "https://japdevdep.github.io/ecommerce-api/cart/987.json";
 const CART_BUY_URL = "https://japdevdep.github.io/ecommerce-api/cart/buy.json";
 
-var showSpinner = function(){
+var showSpinner = function () {
   document.getElementById("spinner-wrapper").style.display = "block";
 }
 
-var hideSpinner = function(){
+var hideSpinner = function () {
   document.getElementById("spinner-wrapper").style.display = "none";
 }
 
-var getJSONData = function(url){
-    var result = {};
-    showSpinner();
-    return fetch(url)
+var getJSONData = function (url) {
+  var result = {};
+  showSpinner();
+  return fetch(url)
     .then(response => {
       if (response.ok) {
         return response.json();
-      }else{
+      } else {
         throw Error(response.statusText);
       }
     })
-    .then(function(response) {
-          result.status = 'ok';
-          result.data = response;
-          hideSpinner();
-          return result;
+    .then(function (response) {
+      result.status = 'ok';
+      result.data = response;
+      hideSpinner();
+      return result;
     })
-    .catch(function(error) {
-        result.status = 'error';
-        result.data = error;
-        hideSpinner();
-        return result;
+    .catch(function (error) {
+      result.status = 'error';
+      result.data = error;
+      hideSpinner();
+      return result;
     });
 }
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function (e) {
+
+
+  let claveUser = localStorage.getItem("User");
+  let nombreUser = document.getElementById("nombreUser");
+  let nombreContenedor = document.getElementById("contenedor");
+
+  if(claveUser){
+    claveUser = JSON.parse(claveUser);
+    nombreUser.innerText = nombreUser.innerText + claveUser.user;
+    nombreContenedor.style = "display: inline-block";
+  }
+  
+  document.getElementById("salir").addEventListener("click", function (e) {
+
+    localStorage.removeItem("User");
+    window.location = "index.html";
+
+  });
+
+  // boton de google 
+
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      window.location = "index.html";
+    });
+  };
+
+  function onLoad() {
+    gapi.load('auth2', function () {
+      gapi.auth2.init();
+    });
+  }
+
 });
--- js/products.js
+++ js/products.js
@@ -1,45 +1,110 @@
+// nuevo filtro
 
-var categoriesArray = [];
+const ORDER_ASC_BY_PRECIO = "MAYOR";
+const ORDER_DESC_BY_PRECIO = "MENOR";
+const ORDER_BY_PROD_SOLDCOUNT = "Cant.";
+var currentCategoriesArray = [];
+var currentSortCriteria = undefined;
+var minCount = undefined;
+var maxCount = undefined;
 
-function showCategoriesList(array){
+function sortCategories(criteria, array) {
+    let result = [];
+    if (criteria === ORDER_ASC_BY_PRECIO) {
+        result = array.sort(function (a, b) {
+            if (a.cost < b.cost) { return -1; }
+            if (a.cost > b.cost) { return 1; }
+            return 0;
+        });
+    } else if (criteria === ORDER_DESC_BY_PRECIO) {
+        result = array.sort(function (a, b) {
+            if (a.cost > b.cost) { return -1; }
+            if (a.cost < b.cost) { return 1; }
+            return 0;
+        });
+    } else if (criteria === ORDER_BY_PROD_SOLDCOUNT) {
+        result = array.sort(function (a, b) {
+            let aCount = parseInt(a.soldCount);
+            let bCount = parseInt(b.soldCount);
+
+            if (aCount > bCount) { return -1; }
+            if (aCount < bCount) { return 1; }
+            return 0;
+        });
+    }
+
+    return result;
+}
+
+function showCategoriesList() {
 
     let htmlContentToAppend = "";
-    for(let i = 0; i < array.length; i++){
-        let category = array[i];
+    for (let i = 0; i < currentCategoriesArray.length; i++) {
+        let category = currentCategoriesArray[i];
 
         htmlContentToAppend += `
         <div class="list-group-item list-group-item-action">
             <div class="row">
                 <div class="col-3">
-                    <img src="` + category.imgSrc + `" alt="` + category.description + `" class="img-thumbnail">
-                </div>
-                <div class="col">
-                    <div class="d-flex w-100 justify-content-between">
-                        <div class="mb-1">
-                        <h4>`+ category.name + " - USD " + category.cost +`</h4>
-                        <p> `+ category.description +` </p>
-                        
-                        </div>
-                        <small class="text-muted">` + category.soldCount + ` artículos</small>
+                     <img src="` + category.imgSrc + `" alt="` + category.description + `" class="img-thumbnail">
+                     </div>
+                        <div class="col">
+                         <div class="d-flex w-100 justify-content-between">
+                          <div class="mb-1">
+                           <h4>`+ category.name + " - USD " + category.cost + `</h4>
+                             <p> `+ category.description + ` </p>
+                                        
+                              </div>
+                              <small class="text-muted">` + category.soldCount + ` artículos</small>
+                            </div>
+        
+                            </div>
+                         </div>
                     </div>
+                   `
 
-                </div>
-            </div>
-        </div>
-        `
 
         document.getElementById("contenido").innerHTML = htmlContentToAppend;
     }
 }
 
-document.addEventListener("DOMContentLoaded", function(e){
-    getJSONData(PRODUCTS_URL).then(function(resultObj){
-        if (resultObj.status === "ok")
-        {
-            categoriesArray = resultObj.data;
-            //Muestro las categorías ordenadas
-            showCategoriesList(categoriesArray);
+
+function sortAndShowCategories(sortCriteria, categoriesArray) {
+    currentSortCriteria = sortCriteria;
+
+    if (categoriesArray != undefined) {
+        currentCategoriesArray = categoriesArray;
+    }
+
+    currentCategoriesArray = sortCategories(currentSortCriteria, currentCategoriesArray);
+
+    //Muestro las categorías ordenadas
+    showCategoriesList();
+}
+
+
+
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCTS_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            sortAndShowCategories(ORDER_ASC_BY_PRECIO, resultObj.data);
         }
     });
+
+    document.getElementById("sortAsc").addEventListener("click", function () {
+        sortAndShowCategories(ORDER_ASC_BY_PRECIO);
+    });
+
+    document.getElementById("sortDesc").addEventListener("click", function () {
+        sortAndShowCategories(ORDER_DESC_BY_PRECIO);
+    });
+
+    document.getElementById("sortByCount").addEventListener("click", function () {
+        sortAndShowCategories(ORDER_BY_PROD_SOLDCOUNT);
+    });
+
+    
 });
+
+
 
