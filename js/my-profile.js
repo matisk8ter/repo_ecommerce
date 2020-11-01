4c8410651f5109ac7ae9b8b1cce7137079ae32aa
Matias Souza <matii-s@hotmail.com>
2020-10-31 21:02:53 -0300

    perfil

--- js/my-profile.js
+++ js/my-profile.js
@@ -3,4 +3,63 @@
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
 
+    let ImgValue = document.getElementById("inputImg");
+    let boton = document.getElementById("boton");
+
+    let nuevoPerfil = localStorage.getItem("perfil")
+
+    if(nuevoPerfil){
+        nuevoPerfil = JSON.parse(nuevoPerfil)
+
+        document.getElementById("nombre").value = nuevoPerfil.name;
+        document.getElementById("apellido").value = nuevoPerfil.ape;
+        document.getElementById("email").value = nuevoPerfil.email;
+        document.getElementById("inputImg").value = nuevoPerfil.avatar;
+
+        if(nuevoPerfil.avatar!= ""){
+
+         document.getElementById("imgSrc").src = nuevoPerfil.avatar;
+        }
+    }
+
+    boton.addEventListener("click", function () {
+
+        let nombre = document.getElementById("nombre");
+        let apellido = document.getElementById("apellido");
+        let email = document.getElementById("email");
+
+        let validacion = true;
+
+        if (nombre.value.trim() === "") {
+            validacion = false
+        }
+
+        if (apellido.value.trim() === "") {
+            validacion = false
+        }
+
+        if (email.value.trim() === "") {
+            validacion = false
+        }
+
+        if (validacion) {
+
+            localStorage.setItem(
+
+                "perfil", JSON.stringify({
+
+                    name: nombre.value,
+                    ape : apellido.value,
+                    email : email.value,
+                    avatar : ImgValue.value
+                }) 
+ 
+            );
+           
+           window.location = "my-profile.html";
+        } 
+
+        
+    });
+
 });
